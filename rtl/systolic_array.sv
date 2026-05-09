a4af11bf8ed84c559228ea5cdb7c2405c2ec2c6e
Jo Choi <cyj19960923@gmail.com>
2026-05-09 18:08:54 -0400

    input skew

--- rtl/systolic_array.sv
+++ rtl/systolic_array.sv
@@ -14,10 +14,28 @@ module systolic_array #(
     output logic signed [31:0]  c_out [N]
 );
 
+    // After input skew, this is what actually enters each row of the array
+    logic signed [15:0] a_skewed [N];
+
     // a_wires[i][j] is the a_out of PE(i,j), which feeds PE(i,j+1)'s a_in
     // c_wires[i][j] is the c_out of PE(i,j), which feeds PE(i+1,j)'s c_in
     logic signed [15:0] a_wires [N][N];
     logic signed [31:0] c_wires [N][N];
+
+
+     // Input skew: row k gets k cycles of delay
+    generate
+        for (i = 0; i < N; i = i + 1) begin: askew_row
+            shift_reg #(.WIDTH(16), .DEPTH(i)) 
+            row_skew (
+                .clk   (clk),
+                .rst   (rst),
+                .d_in  (a_in[i]),
+                .d_out (a_skewed[i])
+            );
+        end
+    endgenerate
+
 
     genvar i, j;
     generate
@@ -27,7 +45,7 @@ module systolic_array #(
                     .clk         (clk),
                     .rst         (rst),
                     .load_weight (load_weight),
-                    .a_in        (j == 0 ? a_in[i]   : a_wires[i][j-1]),
+                    .a_in        (j == 0 ? a_skewed[i]   : a_wires[i][j-1]),
                     .b_in        (b_in[i][j]),
                     .c_in        (i == 0 ? 32'sd0    : c_wires[i-1][j]),
                     .a_out       (a_wires[i][j]),
